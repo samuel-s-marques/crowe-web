143b20c3ee729ee39e54a18befdc701e9f121651
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-17 14:28:01 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueFormulate from '@braid/vue-formulate'
+import VueFormulateExtended from 'vue-formulate-extended'
+import './assets/formulate.css'
+import { pt } from '@braid/vue-formulate-i18n'
+import axios from 'axios'
+import Routes from './routes'
 
+Vue.use(VueFormulate, {
+	plugins: [
+		VueFormulateExtended({
+			features: {
+				formEvents: true,
+				textMask: true,
+				enforceNumber: false
+			}
+		}),
+		pt
+	],
+	locale: 'pt',
+})
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	routes: Routes,
+	mode: 'history'
+})
+
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	render: h => h(App),
+	router: router
 }).$mount('#app')
