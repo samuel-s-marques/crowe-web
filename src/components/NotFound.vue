7f600c010e82081a3932cbf3b7207fd0432d1de9
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-18 18:07:47 -0300

    criado pagina de 404

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+	<b-container>
+		<b-row class="vh-100 text-center" align-v="center">
+			<b-col>
+				<span class="display-1 d-block">404</span>
+				<div class="mb-4 lead">Página não encontrada.</div>
+				<b-link href="/">Voltar ao inicio</b-link>
+			</b-col>
+		</b-row>
+	</b-container>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
