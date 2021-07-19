afdac0e99d206236761988a5e98ddfd64145b97e
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-18 22:40:02 -0300

    navbar ajustada ao container

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,18 +1,21 @@
 <template>
 	<div id="navbar">
 		<b-navbar toggleable="lg" variant="light">
-			<b-navbar-brand href="/">
-				<img src="../assets/logo-crowe-full.png" width="auto" height="28" class="d-inline-block align-top" alt="Crowe logo">
-			</b-navbar-brand>
+			<b-container>
+				<b-navbar-brand href="/">
+					<img src="../assets/logo-crowe-full.png" width="auto" height="28" class="d-inline-block align-top" alt="Crowe logo">
+				</b-navbar-brand>
 
-			<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+				<b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-			<b-collapse id="nav-collapse" is-nav>
-				<b-navbar-nav>
-					<b-nav-item href="/cadastro">Cadastro</b-nav-item>
-					<b-nav-item href="/">Dados</b-nav-item>
-				</b-navbar-nav>
-			</b-collapse>
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav>
+						<b-nav-item href="/cadastro">Cadastro</b-nav-item>
+						<b-nav-item href="/">Dados</b-nav-item>
+					</b-navbar-nav>
+				</b-collapse>
+
+			</b-container>
 		</b-navbar>
 	</div>
 </template>
