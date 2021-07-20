3ca703508a07b6f80287c9d400d5fccaedca22e9
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-07-20 15:13:38 -0300

    removido axios do arquivo, ja que não é utilizado

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import axios from 'axios'
 import Routes from './routes'
 import VueTheMask from 'vue-the-mask'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
